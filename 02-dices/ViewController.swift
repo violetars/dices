46d6202077c25d80e6fdba4a7e2ba1918be527cc
Violeta <violetarecio@gmail.com>
2020-09-15 11:16:40 +0200

    Initial Commit

---
+++ 02-dices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  02-dices
+//
+//  Created by Violeta Recio Sansón on 15/09/2020.
+//  Copyright © 2020 Violeta Recio Sansón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
